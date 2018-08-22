c6ed83f8dd0ee7bb75ddb9d4a9a26362cb35a653
Alan Ficagna <alan.ficagna@gmail.com>
2018-08-22 21:45:47 +0000

    First draft.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source 'http://rubygems.org'
 gem "faraday", "~> 0.15.2"
 
 gem "slop", "~> 4.6"
+
+gem "nokogiri", "~> 1.8"
+
+gem "pry", "~> 0.11.3"
--- weather.rb
+++ weather.rb
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
 require 'slop'
+require 'pry'
+require 'faraday'
+require 'nokogiri'
 
 ARGV << '-h' if ARGV.empty?
 
@@ -15,3 +19,18 @@ opts = Slop.parse do |o|
     puts o
   end
 end
+
+index = Faraday.get('http://api.tiempo.com/index.php?api_lang=es&division=102&affiliate_id=zdo2c683olan')
+index_response = Nokogiri::XML(index.body)
+
+city_uri = index_response.at('data:contains("Tona")').css('url').text
+
+city = Faraday.get("#{city_uri}&affiliate_id=zdo2c683olan")
+city_response = Nokogiri::XML(city.body)
+
+
+mins = city_response.css('var:contains("Temperatura Mínima")').css('data').css('forecast').collect(&:values).map(&:last)
+maxs = city_response.css('var:contains("Temperatura Máxima")').css('data').css('forecast').collect(&:values).map(&:last)
+
+puts maxs
+puts mins
