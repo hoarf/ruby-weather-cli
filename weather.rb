5667558db999d14659379d59de3a4d8e1dce15b8
Alan Ficagna <alan.ficagna@gmail.com>
2018-09-15 09:18:33 +0000

    Wip

--- weather.rb
+++ weather.rb
@@ -1,36 +1,48 @@
 # -*- coding: utf-8 -*-
-require 'slop'
-require 'pry'
-require 'faraday'
-require 'nokogiri'
+require "pry"
+require "faraday"
+require "nokogiri"
+require "optparse"
+require_relative "controller"
 
-ARGV << '-h' if ARGV.empty?
+ARGV << "-h" if ARGV.empty?
 
-opts = Slop.parse do |o|
+FARADAY_WEATHER_ENDPOINT = "http://api.tiempo.com/index.php?api_lang=es&division=102&affiliate_id=zdo2c683olan"
+
+OptionParser.new  do |o|
   o.banner = "Usage: weather [options] | command city"
-  o.separator ''
-  o.separator 'commands:'
-  o.string '-today' , 'Shows the todays temperature for the city.'
-  o.string '-av_max', 'Shows the minimum temperature for the week for the city.'
-  o.string '-av_min', 'Shows the maximum temperature for the week for the city.'
-  o.separator ''
-  o.separator 'options:'
-  o.on('-h', '--help') do
+  o.separator ""
+  o.on "-t", "--today", "Shows today's temperature for the city."
+  o.on "-M", "--max", "Shows the maximum temperature for the week for the city."
+  o.on "-m", "--min", "Shows the minimum temperature for the week for the city."
+  o.separator ""
+  o.separator "Options:"
+  o.on("-h", "--help", "Show help") do
     puts o
   end
-end
+end.parse!
 
-index = Faraday.get('http://api.tiempo.com/index.php?api_lang=es&division=102&affiliate_id=zdo2c683olan')
-index_response = Nokogiri::XML(index.body)
+# index = Faraday.get(FARADAY_WEATHER_ENDPOINT)
+# index_response = Nokogiri::XML(index.body)
 
-city_uri = index_response.at('data:contains("Tona")').css('url').text
+# city_uri = index_response.at('data:contains("Tona")').css("url").text
 
-city = Faraday.get("#{city_uri}&affiliate_id=zdo2c683olan")
-city_response = Nokogiri::XML(city.body)
+# city = Faraday.get("#{city_uri}&affiliate_id=zdo2c683olan")
+# city_response = Nokogiri::XML(city.body)
 
+# puts city_response
+# mins = city_response
+#   .css('var:contains("Temperatura Mínima")')
+#   .css("data")
+#   .css("forecast")
+#   .collect(&:values)
+#   .map(&:last)
 
-mins = city_response.css('var:contains("Temperatura Mínima")').css('data').css('forecast').collect(&:values).map(&:last)
-maxs = city_response.css('var:contains("Temperatura Máxima")').css('data').css('forecast').collect(&:values).map(&:last)
+# maxs = city_response
+#   .css('var:contains("Temperatura Máxima")')
+#   .css("data")
+#   .css("forecast")
+#   .collect(&:values)
+#   .map(&:last)
 
-puts maxs
-puts mins
+# puts mins.first, maxs.first if args[:today]
