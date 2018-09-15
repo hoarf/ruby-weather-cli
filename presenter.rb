e3dd0331db6bf1d3c02585f24c6724a2006e7470
Alan Ficagna <alan.ficagna@gmail.com>
2018-09-15 13:38:43 -0300

    Add a presenter to the reporter.

---
+++ presenter.rb
@@ -0,0 +1,18 @@
+class WeatherPresenter
+
+  def initialize(reporter)
+    @reporter = reporter
+  end
+
+  def max() present_float(@reporter.max) end
+
+  def min() present_float(@reporter.min) end
+
+  def today_max() present_float(@reporter.today_max) end
+
+  def today_min() present_float(@reporter.today_min) end
+
+  private
+
+  def present_float(float) "#{'%0.2f' % float}" end
+end
